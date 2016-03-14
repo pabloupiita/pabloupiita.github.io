af8ca70d0f5b629aac83598a9f9fab8c2ea6e2e8
pabloupiita <meca.adolfo@gmail.com>
2016-03-13 23:39:23 -0600

    Update eje_eje2.js

--- rv/eje_eje2.js
+++ rv/eje_eje2.js
@@ -1,6 +1,6 @@
 function setup(){
 var forma =new THREE.BoxGeometry(1,1,1);
-var material= new THREE.MeshNormalMaterial({color: 0x00ff00});
+var material= new THREE.MeshNormalMaterial();
 malla= new THREE.Mesh(forma,material);
 
 escena =new THREE.Scene();
