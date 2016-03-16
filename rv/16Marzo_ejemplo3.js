c40d3a1c6bce3616245fdffa91d7d47609286c22
pabloupiita <meca.adolfo@gmail.com>
2016-03-16 10:58:06 -0600

    Update 16Marzo_ejemplo3.js

--- rv/16Marzo_ejemplo3.js
+++ rv/16Marzo_ejemplo3.js
@@ -4,7 +4,6 @@ var textura = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures
 var material= new THREE.MeshLambertMaterial({map: textura});
 var forma =new THREE.BoxGeometry(1,1,1);
 malla= new THREE.Mesh(forma,material);
-malla.scale.set(1,4,9);
 
 var luzPuntual = new THREE.PointLight(0xFFFFFF);
 luzPuntual.position.x=10;
