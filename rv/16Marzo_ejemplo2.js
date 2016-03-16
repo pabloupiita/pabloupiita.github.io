a74bf195836f9255bb4409170a51b926d1cd6d97
pabloupiita <meca.adolfo@gmail.com>
2016-03-16 10:38:43 -0600

    Update 16Marzo_ejemplo2.js

--- rv/16Marzo_ejemplo2.js
+++ rv/16Marzo_ejemplo2.js
@@ -7,8 +7,8 @@ malla= new THREE.Mesh(forma,material);
 
 var luzPuntual = new THREE.PointLight(0xFFFFFF);
 luzPuntual.position.x=10;
-luzPuntual.position.x=10;
-luzPuntual.position.x=10;
+luzPuntual.position.y=10;
+luzPuntual.position.z=10;
 
 escena =new THREE.Scene();
 escena.add(malla);
