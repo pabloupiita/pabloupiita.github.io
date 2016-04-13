a56d2fcd46a68dae7958fcec7574ee3b5ec223f8
pabloupiita <meca.adolfo@gmail.com>
2016-04-13 11:17:14 -0500

    Create Robots-agentes3.js

---
+++ rv/Robots-agentes3.js
@@ -0,0 +1,72 @@
+function Robot(size,x,y){
+	Agent.call(this,x,y);
+}
+
+	this.sensor=new Sensor();
+	this.actuato= new THREE.Mesh(
+	new THREE.BoxGeometry(size,size,size),
+		new THREE.MeshBasicMaterial({color: '#aa0000'}));
+	this.actuator.commands=[];
+	this.add(this.actuator);
+}
+
+Robot.prototype=new Agent();
+
+Robot.prototype.sense=function(environment){
+	this.sensor.set(this.position,
+		new THREE.Vector3(Math.cos(this.rotation.z),
+			Math.sin(this.rotation.z),
+			0));
+	var obstaculo=this.sensor.intersectObjects(environment.children,true);
+
+	if((obstaculo.length > 0 &&
+	(obstaculo[0].distance<=.5)))
+		this.sensor.colision=true;
+	else
+		this.sensor.colision=false;
+};
+
+Robot.prototype.plan=function(environment){
+	this.actuator.commands=[];
+
+	if(this.sensor.colision==true)
+	this.actuator.commands.push('rotateCCW');
+	else
+	this.actuator.commands.push('goStraight');
+};
+
+
+Robot.prototype.act=function(environment){
+	var command=this.actuator.commands.pop();
+
+	if(command===undefined)
+	console.log('Undefined command');
+	else if(commmand in this.operations)
+	this.operations[command](this)
+	else
+	console.log('Unknown command');
+};
+
+
+
+//Las posibles operaciones con este robot son 
+//goStraight
+//rotateCW
+//rotateCCW
+
+Robot.prototype.operations={};
+
+Robot.prototype.operations.goStraight=function(robot, distance){
+	if(distance===undefined)
+	distance=0.5;
+	robot.position.x +=distance*Math.cos(robot.rotation.z);
+	robot.position.y +=distance*Math.sin(robot.rotation.z);
+};
+
+
+Robot.prototype.operations.rotateCW=function(robot,angle){
+	if(angle===undefined)
+	angle=.Math.PI/2;
+	robot.rotation.z+=angle;
+};
+
